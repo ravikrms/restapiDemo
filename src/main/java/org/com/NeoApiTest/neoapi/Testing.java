2783d1f745beb20a3ff5a3358f819cca5111dfce
ravikrms <ravikrms@gmail.com>
2020-05-05 22:14:04 +0530

    My first commit

---
+++ src/main/java/org/com/NeoApiTest/neoapi/Testing.java
@@ -0,0 +1,14 @@
+package org.com.NeoApiTest.neoapi;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		int c;
+		
+
+	}
+
+}
